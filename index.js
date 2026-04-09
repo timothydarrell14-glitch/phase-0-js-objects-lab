fb2f3c3224115bd564cf60819e679249d503a031
timothydarrell14-glitch <timothydarrell14@gmail.com>
2026-04-09 13:26:32 +0300

    ADD: implement attendee object and related functions for event management

--- index.js
+++ index.js
@@ -1,6 +1,58 @@
 //Write your code here
 
+let attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00,
+}
 
+
+
+function logAttendeeName(attendee){
+  let name = attendee.name
+  return name 
+}
+console.log(logAttendeeName(attendee.name))
+
+
+
+function logTicketPrice(attendee){
+  let price = attendee.ticketPrice
+  return price
+}
+console.log(logAttendeeName(attendee.ticketPrice))
+
+
+
+function updateTicketType(attendee){
+  attendee.ticketType = ["Regular"]
+}
+console.log(updateTicketType)
+
+
+
+function updateTicketPrice(attendee){
+  attendee.ticketPrice = [100]
+}
+console.log(updateTicketPrice)
+
+
+
+function removeEventProperty(attendee){
+  delete attendee.event
+}
+
+
+
+function addCheckedInProperty(attendee){
+  attendee.checkedIn = "True"
+}
+console.log(addCheckedInProperty(attendee))
+
+
+console.log(attendee)
 
 //Needed for the tests to work. Don't modify
 module.exports = {
